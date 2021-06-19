eef46959dbf77a769ed2f49dc98aa7cd0e3dd0ab
Nazmul Hassan <nazmul.phpdroidx@gmail.com>
2021-06-19 17:16:52 +0600

    current user in jwt except pass digest

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,5 +15,6 @@ class ApplicationController < ActionController::API
   # Check for valid request token and return user
   def authorize_request
     @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+    @token = request.headers['Authorization']
   end
 end
