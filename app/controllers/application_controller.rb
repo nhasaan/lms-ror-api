edc1d0aad26778df6efc245b375750e72f57dd1f
Nazmul Hassan <nazmul.phpdroidx@gmail.com>
2021-06-19 17:08:41 +0600

    current user in jwt context

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ class ApplicationController < ActionController::API
   before_action :authorize_request
   attr_reader :current_user
 
+  def me
+    render json: @current_user.attributes.except('password_digest')
+  end
+
   private
 
   # Check for valid request token and return user
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     resources :lessons
   end
   post 'auth/login', to: 'authentication#authenticate'
-  post 'auth/me', to: 'users#current_user'
+  post 'auth/me', to: 'application#me'
   post 'signup', to: 'users#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
